2daf6146d5d22bf69a83bc70ca2522f1f44b60c4
Stas <stas.krivosheev.0054@gmail.com>
2024-07-22 20:22:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, } from 'react';
 import List from './Components/List';
+import MyContext from './Components/My_context';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   const [data, setData] = useState([
     { id: crypto.randomUUID(), value: 'Item 1' },
     { id: crypto.randomUUID(), value: 'Item 2' },
@@ -15,7 +22,7 @@ function App() {
   const handleAddItem = () => {
     if (newItemValue.trim() !== '') {
       const newId = crypto.randomUUID();
-      setData(data =>[...data, { id: newId, value: newItemValue }])
+      setData(data => [...data, { id: newId, value: newItemValue }])
       setNewItemValue('');
       inputRef.current.focus();
     }
@@ -28,61 +35,87 @@ function App() {
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '100vh',
-        backgroundColor: '#8b58ff',
-      }}
-    >
+    <MyContext.Provider value={theme}>
       <div
         style={{
-          backgroundColor: 'white',
-          padding: '2rem',
-          borderRadius: '8px',
-          boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          backgroundColor: theme === 'light' ? 'white' : 'black',
+          color: theme === 'light' ? 'black' : 'white',
+          transition: 'background-color 0.3s ease, color 0.3s ease',
         }}
       >
-        <h1>My List App</h1>
-        <div style={{ marginBottom: '1rem' }}>
-          <input
-            type="text"
-            ref={inputRef}
-            value={newItemValue}
-            onChange={(e) => setNewItemValue(e.target.value)}
-            onKeyDown={handleKeyPress}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-            placeholder="Введите новый элемент"
-            style={{
-              padding: '0.5rem',
-              marginRight: '0.5rem',
-              borderRadius: '4px',
-              border: `1px solid ${isFocused ? '#8b58ff' : '#ccc'}`,
-            }}
-          />
-          <button
-            onClick={handleAddItem}
-            onMouseDown={() => setIsButtonActive(true)}
-            onMouseUp={() => setIsButtonActive(false)}
-            style={{
-              padding: '0.5rem 1rem',
-              backgroundColor: `${isButtonActive ? '#6f3de0' : '#8b58ff'}`,
-              color: 'white',
+        <div
+          style={{
+            backgroundColor: theme === 'light' ? 'white' : '#333',
+            padding: '2rem',
+            borderRadius: '8px',
+            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+            transition: 'background-color 0.3s ease, color 0.3s ease',
+          }}
+        >
+          <h1>My List App</h1>
+          <div style={{ marginBottom: '1rem' }}>
+            <input
+              type="text"
+              ref={inputRef}
+              value={newItemValue}
+              onChange={(e) => setNewItemValue(e.target.value)}
+              onKeyDown={handleKeyPress}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              placeholder="Введите новый элемент"
+              style={{
+                padding: '0.5rem',
+                marginRight: '0.5rem',
+                borderRadius: '4px',
+                border: `1px solid ${isFocused ? '#8b58ff' : '#ccc'}`,
+                backgroundColor: theme === 'light' ? 'white' : '#333',
+                color: theme === 'light' ? 'black' : 'white',
+                transition: 'background-color 0.3s ease, color 0.3s ease',
+              }}
+            />
+            <button
+              onClick={handleAddItem}
+              onMouseDown={() => setIsButtonActive(true)}
+              onMouseUp={() => setIsButtonActive(false)}
+              style={{
+                padding: '0.5rem 1rem',
+                backgroundColor: `${isButtonActive ? '#6f3de0' : '#8b58ff'}`,
+                color: 'white',
+                border: 'none',
+                borderRadius: '4px',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s ease, color 0.3s ease',
+              }}
+            >
+              Добавить
+            </button>
+            <div className={`test-container ${theme}`}>
+              <h1>Test Component</h1>
+              <button
+                onClick={toggleTheme}
+                style={{
+              backgroundColor: theme === 'light' ? 'black' : 'white',
+              color: theme === 'light' ? 'white' : 'black',
+              padding: '10px 20px',
               border: 'none',
-              borderRadius: '4px',
               cursor: 'pointer',
-            }}
-          >
-            Добавить
-          </button>
+              marginLeft: '60px',
+              transition: 'background-color 0.3s ease, color 0.3s ease',
+                }}
+              >
+              Toggle Theme
+            </button>
+          </div>
         </div>
         <List data={data} />
       </div>
     </div>
+    </MyContext.Provider >
   );
 }
 
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,12 +1,15 @@
-import { useState,useEffect, useCallback, useMemo } from 'react';
+import { useState, useEffect, useCallback, useMemo, useContext } from 'react';
 import ListItem from './ListItem';
-
+import MyContext from './My_context';
 function List({ data }) {
+
+  const value = useContext(MyContext);
+  console.log(`Дочерний компонент ${value}`);
   const [items, setItems] = useState(data);
 
-  useEffect(()=>{
-setItems(data)
-  },[data])
+  useEffect(() => {
+    setItems(data)
+  }, [data])
 
   const handleClick = useCallback((id) => {
     setItems((prevItems) =>
